389b8f25234de2244d7f0ac1ab9a9e9e44b61652
LUTFU <dolaylutfu@gmail.com>
2025-11-06 20:17:44 +0300

    adding banner handlers

--- Core/CarBook.Application/Features/CQRS/Commands/BannerCommands/CreateBannerCommand.cs
+++ Core/CarBook.Application/Features/CQRS/Commands/BannerCommands/CreateBannerCommand.cs
@@ -8,6 +8,9 @@ namespace CarBook.Application.Features.CQRS.Commands.BannerCommands
 {
     public class CreateBannerCommand
     {
-
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string VideoDescription { get; set; }
+        public string VideoUrl { get; set; }
     }
 }
